30620517326b2262efe6645cf1906126dae4361d
L716240 <lorenzana.marcus@heb.com>
2017-04-04 12:44:40 -0500

    making successful requests to ticket master api

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var http = require("http");
+var url ="app.ticketmaster.com";
 
 router.get('/',function(req,res){
     res.send('Test index');
@@ -7,6 +9,20 @@ router.get('/',function(req,res){
 
 router.get('/newsfeed',function(req,res){
     res.send('Test newsfeed');
+    var options = {
+        host: url,
+        path: "/discovery/v2/events.json?keyword=kanye&apikey=FXtcvJb7gP7JHkHApAMrS8zToFfbqSYR",
+        method: 'GET'
+    };
+
+    http.request(options, function(res) {
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+        });
+    }).end();
 });
 
 module.exports = router; 
