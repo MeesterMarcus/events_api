ca7f11f557b59a66386530d76cd3505864a0825f
L716240 <lorenzana.marcus@heb.com>
2017-04-04 14:24:46 -0500

    testing mongo db

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 var express = require('express');
 var app = express();
-
 var routes = require('./routes/index');
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/events_db';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  insertDocuments(db, function() {
+    db.close();
+  });
+});
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
 
 app.use('/',routes);
 
