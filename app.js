545de338a11e2e1c0b6bdb1f4fec70b030254515
L716240 <lorenzana.marcus@heb.com>
2017-05-16 12:34:53 -0500

    testing inserts

--- app.js
+++ app.js
@@ -3,38 +3,69 @@ var app = express();
 var routes = require('./routes/index');
 
 var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+    , assert = require('assert');
 
 // Connection URL
 var url = 'mongodb://localhost:27017/events_db';
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+MongoClient.connect(url, function (err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
 
-  insertDocuments(db, function() {
-    db.close();
-  });
+    insertUsers(db, function () {
+        db.close();
+    });
+
+    insertEvents(db, function () {
+        db.close();
+    });
 });
 
-var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
+var insertUsers = function (db, callback) {
+    // Get the documents collection
+    var collection = db.collection('users');
+    // Insert some documents
+    collection.insertMany([
+        {
+            firstName: "Jim",
+            lastName: "Smith",
+            email: "jimsmith@aol.com",
+            username: "smithyjimmy",
+            password: "abc123",
+            eventIds: [],
+            genres: ["Pop"],
+            userIds: []
+        }
+    ], function (err, result) {
+        callback(result);
+    });
 }
 
-app.use('/',routes);
+var insertEvents = function (db, callback) {
+    // Get the documents collection
+    var collection = db.collection('events');
+    // Insert some documents
+    collection.insertMany([
+        {
+            description: "Kanye Concert",
+            street: "123 Some Street",
+            city: "San Antonio",
+            state: "TX",
+            zip: "78204",
+            genres: ["Rap", "Hip-hop"],
+            attendees: [],
+            date: "12/12/12"
+
+
+        }
+    ], function (err, result) {
+        callback(result);
+    });
+}
+
+app.use('/', routes);
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
 });
